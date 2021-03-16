99dbfb784011b9df3162480d7b6433483bf0a417
Veles-Pan <73409754+Veles-Pan@users.noreply.github.com>
2021-03-16 12:49:43 +0300

    Add files via upload

---
+++ src/components/CardTemplate.vue
@@ -0,0 +1,51 @@
+<template>
+<div class="card card_unactive" @click="openCard" :style="{backgroundColor: isActive ? cardColor.color : '#6c6874'}"></div>
+</template>
+
+<script>
+
+export default {
+  name: 'CardTemplate',
+
+    props: {
+        cardColor: Object
+    },
+
+  data() {
+      return {
+          isActive: this.cardColor.isActive
+      }
+  },
+
+  methods: {
+      openCard(evt) {
+            this.isActive = !this.isActive;
+            this.$emit('openedCard', evt.target, this.cardColor, this.isActive)
+      },
+  }
+
+}
+</script>
+
+<style scoped>
+
+
+.card {
+    width: 100px;
+    height: 150px;
+    border-radius: 10px;
+}
+
+.card_finished {
+    opacity: .2;
+    pointer-events: none;
+}
+
+@media screen and (max-width: 1000px) {
+    
+.card {
+    width: 60px;
+    height: 100px;
+    }
+}
+</style>
